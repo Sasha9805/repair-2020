e6bd5603832b82f24bb9898c377b780ef25409c7
Sasha9805 <pimenovs33@gmail.com>
2020-06-26 18:30:23 +0300

    Подключена jquery, сверстано с пом. нее модальное окно, добавлена кнопка прокрутки вверх

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,52 @@
-// Кнопка в шапке
-var btn = document.querySelector("#btn");
-// Крестик
-var close = document.querySelector("#close");
-// Модально окно .modal
-var modal = document.querySelector("#modal");
-// Показ мод. окна
-btn.addEventListener("click", function() {
-  modal.classList.add("modal_visible");
-  // console.log(modal.classList);
-  // console.log("click");
+// // Кнопка в шапке
+// var btn = document.querySelector("#btn");
+// // Крестик
+// var close = document.querySelector("#close");
+// // Модально окно .modal
+// var modal = document.querySelector("#modal");
+// // Показ мод. окна
+// btn.addEventListener("click", function() {
+//   modal.classList.add("modal_visible");
+//   // console.log(modal.classList);
+//   // console.log("click");
+// });
+// // Скрытие мод. окна
+// close.addEventListener("click", function() {
+//   modal.classList.remove("modal_visible");
+// });
+// // console.log(btn);
+
+// Используем jQuery
+$(document).ready(function() {
+  $('#btn').click(function() {
+    $('#modal').addClass('modal_visible');
+  });
+  $('#close').click(function() {
+    $('#modal').removeClass('modal_visible');
+  });
+  // console.log($('#btn'));
+
+  // Плавная прокрутка вверх
+  // Координата верхней части секции offer
+  var offsetTopOffer = $('.offer').offset().top;
+  // Координата верхняя при прокрутке
+  // window and document в данном случае работают одинаково
+  var top = $(window).scrollTop();
+  // var top = $(document).scrollTop();
+  // console.log(top + ' ' + offsetTopOffer + ' ' + typeof offsetTopOffer);
+  // Отлавливание события скролла окна документа
+  $(window).scroll(function() {
+    // top = $(document).scrollTop();
+    top = $(window).scrollTop();
+    // console.log(top);
+    if (top >= (offsetTopOffer - 400)) {
+      $('.arrow-up').addClass('arrow-up_visible')
+    } else {
+      $('.arrow-up').removeClass('arrow-up_visible')
+    }
+  });
+  // Клик на кнопку "вверх", переносимся в начало страницы
+  $('.arrow-up').click(function() {
+    $('html').animate({scrollTop: 0}, 1000);
+  });
 });
-// Скрытие мод. окна
-close.addEventListener("click", function() {
-  modal.classList.remove("modal_visible");
-});
-// console.log(btn);
